c0755cc4b4966494fc7b1a10e4307f9b0e347052
Kris Schmarr <kschmarr@gmail.com>
2019-05-31 15:03:41 -0600

    prepared to deploy

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const articlesRouter = require("./articles/articles-router");
 
 const app = express();
 
@@ -12,6 +13,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+app.use("/api/articles", articlesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
